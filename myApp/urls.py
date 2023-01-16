7c9dd444f77e1ba26fe8400691934e7b19f6cbbc
Bobirbek <siddiqovbobirbekman@gmail.com>
2023-01-16 17:45:38 +0500

    second

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, uz, ru, en
 
 urlpatterns = [
     path("", home, name="home"),
+    path("uz/", uz, name="uzbek"),
+    path("ru/", ru, name="russian"),
+    path("en/", en, name="english"),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, "home.html", context)
+
+def uz(request):
+    context = {}
+    return render(request, "uz.html", context)
+
+def ru(request):
+    context = {}
+    return render(request, "ru.html", context)
+
+def en(request):
+    context = {}
+    return render(request, "eng.html", context)
+
+    
